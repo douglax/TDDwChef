c5fd31f4b6fb7f368502eef7fffc3231ff473c5f
Alejandro Acosta <alxacostaa@gmail.com>
2017-02-24 15:07:52 -0700

    Test loop - minor

--- webserver_test/test/smoke/default/default_test.rb
+++ webserver_test/test/smoke/default/default_test.rb
@@ -4,3 +4,20 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+
+describe package 'httpd' do
+  it { should be_installed }
+end
+
+describe service 'httpd' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command 'curl localhost' do
+  its('stdout') { should match /hello/ }
+end
+
+describe port 80 do
+  it { should be_listening }
+end
